01e3decd2eeccd278cedaa137eabbb74b0d58d15
Paul Madsen <hathadar@hotmail.com>
2018-12-09 22:38:32 -0700

    removed debug text from ScreenSelectorBox.cs

--- Assets/Scripts/ScreenSelectorBox.cs
+++ Assets/Scripts/ScreenSelectorBox.cs
@@ -24,24 +24,14 @@ public class ScreenSelectorBox : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftControl)){            
             boxStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Debug.Log("Box start: " + boxStart);            
-
         }
         if (Input.GetMouseButtonUp(0) && Input.GetKey(KeyCode.LeftControl))
         {
-            boxEnd = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Debug.Log("Box end: " + boxEnd);
-            foreach (Transform child in population.transform)
-            {
-                Debug.Log("Found " + child.gameObject.name + " at coord " + child.transform.position + "Screen: " + cam.WorldToScreenPoint(child.transform.position));
-            }
-            Debug.Log("these gameobjects are within the selection window");
+            boxEnd = new Vector2(Input.mousePosition.x, Input.mousePosition.y);                        
             float xDist = Mathf.Abs(boxEnd.x - boxStart.x);
             float yDist = Mathf.Abs(boxEnd.y - boxStart.y);
             Vector2 lowerLeft = new Vector2(Mathf.Min(boxStart.x, boxEnd.x), Mathf.Min(boxStart.y, boxEnd.y));
             Rect box = new Rect(lowerLeft, new Vector2(xDist, yDist));
-            
-            Debug.Log("Rect box is " + box.xMin + "," + box.yMin + " to " + box.xMax + "," + box.yMax);
             foreach (Transform child in population.transform)
             {
                 Vector3 screenPos = cam.WorldToScreenPoint(child.transform.position);
